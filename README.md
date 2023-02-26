31ed9f2d0ae777c6540c6bde25bde27f0451ac23
SevaStopAll <Sevastopallrh@yandex.ru>
2023-02-26 22:06:59 +0400

    2. Создание и запуск нити [#1016 #395422]

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # SevaStopAll-job4j_threads
 
-Here is a repository for learning multithreading programming and its nuances. 
+Here is a repository for learning multithreading programming and its nuances.
+
+Here I would like to learn how to use wait, notify, notify all from Object,
+what is race condition, deadlock etc. 
+I understand why multi-threading needs and want to use it when it's necessary.  
--- src/test/java/ru/job4j/concurrent/ConcurrentOutput.java
+++ src/test/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -2,13 +2,13 @@ package ru.job4j.concurrent;
 
 public class ConcurrentOutput {
     public static void main(String[] args) {
-        Thread another = new Thread(
+        Thread first = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
         Thread second = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
-        another.start();
+        first.start();
         second.start();
         System.out.println(Thread.currentThread().getName());
     }
