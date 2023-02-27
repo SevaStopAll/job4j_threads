688427521bafc178292498f76469040796b9ecd0
Sevastopall <Sevastopallrh@yandex.ru>
2023-02-27 08:37:14 +0400

    3. Состояние нити. [#229175] Tried to solve this problem, but my solution, I don't think it's optimal.

---
+++ src/test/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,18 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+            System.out.println("Work is in progress");
+        }
+        System.out.println("Work is done");
+    }
+}
