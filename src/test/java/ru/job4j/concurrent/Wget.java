f8524cd0b2e6a8485d03b989f1df55f088600510
SevaStopAll <Sevastopallrh@yandex.ru>
2023-02-27 21:20:35 +0400

    4. Режим ожидания. [#231217] Created a good looking loading-bar. I want to use it in the future.

---
+++ src/test/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,19 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                    for(int index = 0; index <= 100; index++) {
+                        Thread.sleep(1000);
+                        System.out.print("\rLoading : " + index  + "%");
+                    }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                });
+        thread.start();
+    }
+}
