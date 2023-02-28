812e9badd871094fb892b52dbb27925291d9703f
Sevastopall <Sevastopallrh@yandex.ru>
2023-02-28 09:36:04 +0400

    5. Прерывание нити [#1019] Realized an interruption operation.

--- src/test/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/test/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -1,4 +1,29 @@
 package ru.job4j.concurrent;
 
-public class ConsoleProgress {
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        var process = new char[]{'-', '\\', '|', '/' };
+        while (!Thread.currentThread().isInterrupted()) {
+            for (char ch : process) {
+                try {
+                    Thread.sleep(500);
+                    System.out.print("\r load: " + ch);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args)  {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(5000);
+            progress.interrupt();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
