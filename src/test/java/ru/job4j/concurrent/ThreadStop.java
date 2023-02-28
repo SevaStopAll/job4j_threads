befc0136cb11f8544e0a36971094b94ddd0b5cb9
Sevastopall <Sevastopallrh@yandex.ru>
2023-02-28 11:42:16 +0400

    6. Прерывание блокированной нити. [#267413] It's an example of interruption instead of Exception.

--- src/test/java/ru/job4j/concurrent/ThreadStop.java
+++ src/test/java/ru/job4j/concurrent/ThreadStop.java
@@ -2,16 +2,22 @@ package ru.job4j.concurrent;
 
 public class ThreadStop {
     public static void main(String[] args) throws InterruptedException {
-        Thread thread = new Thread(
+        Thread progress = new Thread(
                 () -> {
-                    int count = 0;
                     while (!Thread.currentThread().isInterrupted()) {
-                        System.out.println(count++);
+                        try {
+                            System.out.println("start ...");
+                            Thread.sleep(10000);
+                        } catch (InterruptedException e) {
+                            System.out.println(Thread.currentThread().isInterrupted());
+                            System.out.println(Thread.currentThread().getState());
+                        }
                     }
                 }
         );
-        thread.start();
+        progress.start();
         Thread.sleep(1000);
-        thread.interrupt();
+        progress.interrupt();
+        progress.join();
     }
 }
