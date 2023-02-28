aefa6a7da8495e0d23916c3bdb0942f288d9647f
Sevastopall <Sevastopallrh@yandex.ru>
2023-02-28 11:44:13 +0400

    It's a frame for Wget class for downloading files from NET.

---
+++ src/test/java/ru/job4j/thread/Wget.java
@@ -0,0 +1,24 @@
+package ru.job4j.thread;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        /* Скачать файл*/
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+}
