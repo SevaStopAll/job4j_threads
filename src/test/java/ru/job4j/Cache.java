bba8f43bac8fa8c570ef9adbea284032d17cd4ce
SevaStopAll <Sevastopallrh@yandex.ru>
2023-03-01 20:14:13 +0400

    1. Синхронизация общих ресурсов. [#1096]

--- src/test/java/ru/job4j/Cache.java
+++ src/test/java/ru/job4j/Cache.java
@@ -1,8 +1,6 @@
 package ru.job4j;
-
 public final class Cache {
     private static Cache cache;
-
     public synchronized static Cache instOf() {
         if (cache == null) {
             cache = new Cache();
