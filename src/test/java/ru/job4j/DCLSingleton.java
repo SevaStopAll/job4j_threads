ecc2888e7e340b6eda28faaa4b86d1feeb563992
Sevastopall <Sevastopallrh@yandex.ru>
2023-03-01 09:12:18 +0400

    2. Модель памяти Java [#267917]

---
+++ src/test/java/ru/job4j/DCLSingleton.java
@@ -0,0 +1,21 @@
+package ru.job4j;
+
+public final class DCLSingleton {
+
+    private volatile static  DCLSingleton inst;
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+
+    private DCLSingleton() {
+    }
+
+}
