dfa028c49706954296894d9bbf5b17127074b834
Sevastopall <Sevastopallrh@yandex.ru>
2023-03-01 09:39:17 +0400

    3. Immutable объекты [#267918] Changed an object to make it Immutable.

---
+++ src/test/java/ru/job4j/linked/Node.java
@@ -0,0 +1,20 @@
+package ru.job4j.linked;
+
+public class Node<T> {
+    private final Node<T> next;
+    private final T value;
+
+    public Node(Node<T> next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+}
