c441f4615f1156285806d1c859340db9683577bb
Sevastopall <Sevastopallrh@yandex.ru>
2023-03-02 12:48:39 +0400

    3. Денежные переводы AccountStorage [#1104] Created add and getById methods.

--- src/main/java/ru/job4j/cash/AccountStorage.java
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -1,28 +1,43 @@
 package ru.job4j.cash;
 
+import net.jcip.annotations.ThreadSafe;
+
 import java.util.HashMap;
 import java.util.Optional;
 
+@ThreadSafe
 public class AccountStorage {
     private final HashMap<Integer, Account> accounts = new HashMap<>();
 
-    public boolean add(Account account) {
-        return false;
+    public synchronized boolean add(Account account) {
+        boolean result = true;
+        if (accounts.putIfAbsent(account.id(), account) != null) {
+            return false;
+        }
+        return result;
     }
 
-    public boolean update(Account account) {
-        return false;
+    public synchronized boolean update(Account account) {
+        boolean result = false;
+        return result;
     }
 
-    public boolean delete(int id) {
-        return false;
+    public synchronized boolean delete(int id) {
+        boolean result = false;
+        return result;
     }
 
-    public Optional<Account> getById(int id) {
+    public synchronized Optional<Account> getById(int id) {
+        for (Account account : accounts.values()) {
+            if (account.id() == id) {
+                return Optional.of(account);
+            }
+        }
         return Optional.empty();
     }
 
-    public boolean transfer(int fromId, int toId, int amount) {
-        return false;
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        boolean result = false;
+        return result;
     }
 }
