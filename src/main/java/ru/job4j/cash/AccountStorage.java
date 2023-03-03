e2e41908e08068d666a51449c95f201f5312bd55
Sevastopall <Sevastopallrh@yandex.ru>
2023-03-03 08:14:18 +0400

    3. Денежные переводы AccountStorage [#1104 #397137] Changed methods for make it shorter.

--- src/main/java/ru/job4j/cash/AccountStorage.java
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -10,49 +10,28 @@ public class AccountStorage {
     private final HashMap<Integer, Account> accounts = new HashMap<>();
 
     public synchronized boolean add(Account account) {
-        boolean result = true;
-        if (accounts.putIfAbsent(account.id(), account) != null) {
-            return false;
-        }
-        return result;
+        return accounts.putIfAbsent(account.id(), account) != null;
     }
 
-    public synchronized boolean update(Account account) {
-        boolean result = false;
-        if (getById(account.id()).isEmpty()) {
-            return false;
-        }
-        accounts.replace(account.id(), account);
-        return result;
+    public synchronized Account update(Account account) {
+        return accounts.replace(account.id(), account);
     }
 
-    public synchronized boolean delete(int id) {
-        boolean result = true;
-        if (getById(id).isEmpty()) {
-            return false;
-        }
-        accounts.remove(id);
-        return result;
+    public synchronized Account delete(int id) {
+        return accounts.remove(id);
     }
 
     public synchronized Optional<Account> getById(int id) {
-        for (Account account : accounts.values()) {
-            if (account.id() == id) {
-                return Optional.of(account);
-            }
-        }
-        return Optional.empty();
+        return Optional.ofNullable(accounts.get(id));
     }
 
     public synchronized boolean transfer(int fromId, int toId, int amount) {
-        boolean result = false;
-        Account accountFrom = getById(fromId).get();
-        Account accountTo = getById(toId).get();
+        boolean result = true;
         if (getById(fromId).isEmpty() || getById(toId).isEmpty()) {
             return false;
         }
-        update(new Account(fromId, accountFrom.amount() - amount));
-        update(new Account(toId, accountTo.amount() + amount));
+        update(new Account(fromId, accounts.get(fromId).amount() - amount));
+        update(new Account(toId, accounts.get(toId).amount() + amount));
         return result;
     }
 }
