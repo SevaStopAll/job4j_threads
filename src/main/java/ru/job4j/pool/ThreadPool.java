e1e3047d5d78cbe4608b78d8a5efe89ad59f1b5e
SevaStopAll <Sevastopallrh@yandex.ru>
2023-03-04 17:50:51 +0400

    1. Реализовать ThreadPool [#1099] Tried to make my own ThreadPool.

---
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -0,0 +1,62 @@
+package ru.job4j.pool;
+
+import ru.job4j.SimpleBlockingQueue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreadPool {
+    private final List<Thread> threads = new LinkedList<>();
+    int size = Runtime.getRuntime().availableProcessors();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(size);
+
+    public ThreadPool() {
+        for (int i = 0; i < size; i++) {
+            threads.add(new Thread ( () -> {
+                        while (!Thread.currentThread().isInterrupted()) {
+                            try {
+                                tasks.poll().run();
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                    })
+                    );
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+    }
+
+    public void work(Runnable job) throws InterruptedException {
+        tasks.offer(job);
+    }
+
+    public void shutdown() {
+        new Thread(
+                () -> {
+                    for (Thread thread : threads) {
+                        thread.interrupt();
+                    }
+                }
+        ).start();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable job = () -> System.out.println("Hi from " + Thread.currentThread().getName());
+        ThreadPool test = new ThreadPool();
+        test.work(job);
+        test.work(job);
+        test.work(job);
+        test.work(job);
+        test.work(job);
+        test.work(job);
+        test.work(job);
+        test.work(job);
+        test.work(job);
+        test.work(job);
+        test.work(job);
+        test.work(job);
+        test.shutdown();
+    }
+}
