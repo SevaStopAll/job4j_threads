e82c39f3e59483083533cc1665a3fab942eb24dd
SevaStopAll <Sevastopallrh@yandex.ru>
2023-03-04 22:05:45 +0400

    2. ExecutorService рассылка почты. [#63097] Met ExecutorService and tried to make a simple EmailNotification sender.

---
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -0,0 +1,28 @@
+package ru.job4j.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+
+    private final ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+    public void emailTo(User user) {
+        pool.submit(() -> {
+            String email = user.getEmail();
+            String subject = String.format("Notification %s to email %s", user.getUsername(), email);
+            String body = String.format("Add a new event to %s", user.getUsername());
+            send(subject, body, email);
+        });
+    }
+
+    public void close() {
+        pool.shutdown();
+    }
+
+    public void send(String subject, String body, String email) {
+    }
+
+}
---
+++ src/main/java/ru/job4j/pool/User.java
@@ -0,0 +1,20 @@
+package ru.job4j.pool;
+
+public class User {
+    private final String username;
+    private final String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
