4d3f21eec24c0e4b8c80f1c61045189fe02b2a78
SevaStopAll <Sevastopallrh@yandex.ru>
2023-03-05 12:14:39 +0400

    0. CAS - операции [#6859 #397364] Changed a little realization and add correct test, showing the real work.

--- src/main/java/ru/job4j/CASCount.java
+++ src/main/java/ru/job4j/CASCount.java
@@ -2,16 +2,18 @@ package ru.job4j;
 
 import net.jcip.annotations.ThreadSafe;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 @ThreadSafe
 public class CASCount {
-    private final AtomicReference<Integer> count = new AtomicReference<>(0);
+    private final AtomicInteger count = new AtomicInteger(0);
 
     public void increment() {
         int value;
-        int current = get();
+        int current;
         do {
+             current = get();
              value = current + 1;
         } while (!count.compareAndSet(current, value));
     }
--- src/test/java/ru/job4j/CASCountTest.java
+++ src/test/java/ru/job4j/CASCountTest.java
@@ -2,17 +2,64 @@ package ru.job4j;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CASCountTest {
-    CASCount test = new CASCount();
 
     @Test
     void whenAdd() {
+        CASCount test = new CASCount();
         test.increment();
         test.increment();
         test.increment();
         assertThat(test.get()).isEqualTo(3);
     }
 
+    @Test
+    void whenAddManyTimes()  {
+        CASCount test = new CASCount();
+        ExecutorService pool = Executors.newFixedThreadPool(4);
+        pool.submit(() -> {
+            for (int i = 0; i < 100; i++) {
+                test.increment();
+            }
+        });
+        pool.submit(() -> {
+            for (int i = 0; i < 100; i++) {
+                test.increment();
+            }
+        });
+        pool.submit(() -> {
+            for (int i = 0; i < 100; i++) {
+                test.increment();
+            }
+        });
+        pool.submit(() -> {
+            for (int i = 0; i < 100; i++) {
+                test.increment();
+            }
+        });
+        pool.submit(() -> {
+            for (int i = 0; i < 100; i++) {
+                test.increment();
+            }
+        });
+        pool.submit(() -> {
+            for (int i = 0; i < 100; i++) {
+                test.increment();
+            }
+        });
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        assertThat(test.get()).isEqualTo(600);
+    }
 }
