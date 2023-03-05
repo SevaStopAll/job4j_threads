ffc6e6d21448d14cb11e9910a676076d3936de23
SevaStopAll <Sevastopallrh@yandex.ru>
2023-03-05 10:49:34 +0400

    2. ExecutorService рассылка почты. [#63097 #397524] Gave time to threads to finish the tasks.

--- src/main/java/ru/job4j/pool/EmailNotification.java
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -20,6 +20,13 @@ public class EmailNotification {
 
     public void close() {
         pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void send(String subject, String body, String email) {
