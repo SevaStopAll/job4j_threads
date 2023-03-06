9594e2494d541afe0d8eb21aa05beb1e8c1ca888
SevaStopAll <Sevastopallrh@yandex.ru>
2023-03-06 21:39:46 +0400

    4. CompletableFuture [#361626] These are examples and frame for my task about CompletableFuture.

---
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -0,0 +1,26 @@
+package ru.job4j.pools;
+
+public class RolColSum {
+    public static class Sums {
+        private int rowSum;
+        private int colSum;
+
+        public int getRowSum() {
+            return rowSum;
+        }
+
+        public int getColSum() {
+            return colSum;
+        }
+
+    public static Sums[] sum(int[][] matrix) {
+        return null;
+    }
+
+    public static Sums[] asyncSum(int[][] matrix) {
+        return null;
+    }
+
+    }
+}
+
